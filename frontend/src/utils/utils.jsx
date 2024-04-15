8aca923a28f984ce23be1e3121a3dc39f45508c3
akovac3 <akovac3@etf.unsa.ba>
2024-04-16 00:49:30 +0200

    D: Final changes in frontend.

--- frontend/src/utils/utils.jsx
+++ frontend/src/utils/utils.jsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from 'react';
+import { format } from 'date-fns';
 
 export const createClassName = (...classes) => {
     let finalClass = ''
@@ -30,4 +31,37 @@ export const useOutsideClick = (callback) => {
     }, [ref]);
 
     return ref;
-};
+}
+
+export const getPaginationParams = (search) => {
+    const page = search.get('page');
+    const size = search.get('size');
+    return { page, size }
+}
+
+export const getFilterParams = (search) => {
+    const contains = search.get('contains')
+    const city = search.get('city');
+    const venue = search.get('venue');
+    const genre = search.get('genre');
+    const time = search.get('time');
+    const date = search.get('date') || format(new Date(), 'yyyy-MM-dd');
+
+    return { city, venue, genre, time, date }
+}
+
+export const handleFilterChange = (search, setSearch, field, value) => {
+    if (value) {
+        search.set(field, value);
+        search.set('page', 1)
+    } else {
+        search.delete(field)
+    }
+    setSearch(search);
+}
+
+export const handlePageChange = (search, setSearch, { page = 1, size = 4 }) => {
+    search.set('page', page)
+    search.set('size', size)
+    setSearch(search)
+}

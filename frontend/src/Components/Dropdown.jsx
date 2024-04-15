bbc468892d1038c83c06508c9cb57f62ffc9ff87
akovac3 <akovac3@etf.unsa.ba>
2024-04-15 18:00:51 +0200

    dropdown

--- frontend/src/Components/Dropdown.jsx
+++ frontend/src/Components/Dropdown.jsx
@@ -11,15 +11,8 @@ const Dropdown = ({ className, children, label, placeholder, value, rightIcon, l
         <div onClick={ () => setOpen(!open) } className={ createClassName("w-full relative flex items-center justify-between py-3", className) } ref={ ref } >
             { cloneElement(label, { active: open, value: value }) }
             { open && (
-<<<<<<< HEAD
                 <div className="absolute top-[110%] w-full bg-neutral-0 z-50 border border-neutral-200 rounded-12 max-h-[220px] p-8 overflow-y-scroll" onClick={ () => setOpen(!open) }>
                     { children }
-=======
-                <div className="absolute top-[110%] w-full bg-neutral-0 z-50 border border-neutral-200 rounded-12 text-neutral-900">
-                    <div className="gap-2 pt-8 px-8 max-h-[220px] overflow-y-scroll" onClick={ () => setOpen(!open) }>
-                        { children }
-                    </div>
->>>>>>> C4-Frontend
                 </div>
             ) }
         </div>
@@ -28,11 +21,7 @@ const Dropdown = ({ className, children, label, placeholder, value, rightIcon, l
 
 const DropdownItem = ({ className, children, onClick }) => {
     return (
-<<<<<<< HEAD
         <div className={ createClassName("text-neutral-900 flex capitalize hover:bg-neutral-100 rounded-8 px-12 py-8 cursor-pointer", className) } onClick={ onClick }>{ children }</div>
-=======
-        <div className={ createClassName("", className) }>{ children }</div>
->>>>>>> C4-Frontend
     )
 }
 
--- frontend/src/Components/card/MovieCard.jsx
+++ frontend/src/Components/card/MovieCard.jsx
@@ -38,16 +38,6 @@ const MovieCard = (props) => {
     }, [movie])
 
     return (
-<<<<<<< HEAD
-        <Card className="h-[395px]">
-            <div className="flex items-center justify-center p-12">
-                <img className="w-[98%] h-[287px] rounded-16" src={ cover } alt="" />
-            </div>
-            <p className="text-[20px]/[24px] font-bold tracking-[0.0085em] pl-12">{ movie.name }</p>
-            <div className="text-neutral-500 font-normal h-[20px] tracking-[0.0125em] text-[14px]/[20px] flex p-[10px] pl-12">
-                <p className="border-neutral-400 w-64 h-[20px] border-r mr-12">{ movie.duration } MIN</p>
-                <p>{ movie.genres[0].name }</p>
-=======
         <Card>
             <div className="p-[10px]">
                 <div className="flex items-center justify-center relative">
@@ -59,7 +49,6 @@ const MovieCard = (props) => {
                     <p className="border-neutral-400 w-64 h-[20px] border-r mr-12">{ movie.duration } MIN</p>
                     <p>{ movie.genres[0].name }</p>
                 </div>
->>>>>>> C4-Frontend
             </div>
         </Card>
     )

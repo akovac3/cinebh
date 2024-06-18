f9a9edef31e1a73894f906318259c662b9a1ac7f
akovac3 <akovac3@etf.unsa.ba>
2024-06-18 11:46:39 +0200

    number of reservations.

--- frontend/src/routes/user/UserProfile.jsx
+++ frontend/src/routes/user/UserProfile.jsx
@@ -1,10 +1,36 @@
+import { useState, useEffect, createContext } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock, faFilm, faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 import SideBar from "../../components/SideBar";
 
+import { reservation, url } from "../../utils/api";
+
+export const NumberOfElementsContext = createContext();
+
 const UserProfile = () => {
+    const [numberOfReservations, setNumberOfReservations] = useState(0);
+
+    const getNumberOfReservations = async () => {
+        const token = localStorage.getItem("token");
+        try {
+            const result = await axios.get(`${url}${reservation}/user/count`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            setNumberOfReservations(result.data);
+        } catch (err) {
+            console.log("Error fetching reservations count:", err);
+        }
+    }
+
+    useEffect(() => {
+        getNumberOfReservations()
+    }, [numberOfReservations])
+
     return (
         <div className="flex font-body text-body-l">
             <SideBar position="left" className="w-[264px] flex flex-col gap-32 p-32 text-neutral-300">
@@ -37,7 +63,7 @@ const UserProfile = () => {
                     className={ ({ isActive }) => isActive ? "font-semibold text-neutral-0 shadow-light-400 underline" : "" }
                 >
                     <FontAwesomeIcon icon={ faClock } />
-                    <span className="pl-4">Pending reservations</span>
+                    <span className="pl-4">Pending reservations ({ numberOfReservations })</span>
                 </NavLink>
                 <NavLink
                     to="/user-profile/projections"
@@ -47,9 +73,14 @@ const UserProfile = () => {
                     <span className="pl-8">Projections</span>
                 </NavLink>
             </SideBar>
-            <div className="pl-[264px] w-full">
-                <Outlet />
-            </div>
+            <NumberOfElementsContext.Provider value={ {
+                numberOfReservations,
+                setNumberOfReservations
+            } }>
+                <div className="pl-[264px] w-full">
+                    <Outlet />
+                </div>
+            </NumberOfElementsContext.Provider>
         </div>
     )
 }
